c3cd2815ce43503cc0e97b8a72c86f742c432bf1
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-08-12 16:00:52 -0400

    Ensure LocalLoadPurchases does not delete empty cache .

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -33,6 +33,23 @@ describe('LocalSavePurchases', () => {
    expect(purchases).toEqual([])
  })
 
+
+ test('Should return an empty list if cache is empty', async () => {
+   const currentDate = new Date()
+   const timestamp = new Date(currentDate)
+   timestamp.setDate(timestamp.getDate() - 3)
+   timestamp.setSeconds(timestamp.getSeconds() + 1)
+   const { cacheStore, sut } = makeSut(currentDate)
+   cacheStore.fetchResult = {
+     timestamp,
+     value: [] 
+   }
+   const purchases = await sut.loadAll()
+   expect(cacheStore.fetchKey).toBe('purchases')
+   expect(cacheStore.Actions).toEqual([CacheStoreSpy.Action.fetch])
+   expect(purchases).toEqual([])
+ })
+
  test('Should return a list of purchases if cache is less than 3 days old', async () => {
    const currentDate = new Date()
    const timestamp = new Date(currentDate)
